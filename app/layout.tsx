c9649fa2cd3d8c75fb1f6cb742e4bf70c6de0e7d
canvasclasses <royalmaddy07@gmail.com>
2026-01-24 11:34:59 +0530

    vercel analytics package added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -129,6 +130,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
